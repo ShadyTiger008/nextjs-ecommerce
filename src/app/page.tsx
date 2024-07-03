61b2331302182d001811485e5084d696d83d247f
ShadyTiger008 <chatterjeesoumyajeet@gmail.com>
2024-07-04 00:09:16 +0530

    homepage structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-export default function Dashboard() {
+import Hero from "~/components/@homepage/hero";
+import ShopByCategory from "~/components/@homepage/shop-by-category";
+import TopProducts from "~/components/@homepage/top-products";
+import Footer from "~/components/shared/footer";
+import Header from "~/components/shared/header";
+import Navbar from "~/components/shared/navbar";
+import Newsletter from "~/components/shared/newsletter";
+
+export default function Homepage() {
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-      <div className="flex items-center">
-        <h1 className="text-lg font-semibold md:text-2xl">Landing Page</h1>
-      </div>
+      <Header />
+      <Navbar />
+      <Hero />
+      <ShopByCategory />
+      <TopProducts />
+      <Newsletter />
+      <Footer />
     </main>
   );
 }
